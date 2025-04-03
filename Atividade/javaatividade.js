ba7156921bd32e6912aa8896e7a970844a991bf6
VINÍCIUS GOMES SENS <vinicius_sens@estudante.sesisenai.org.br>
2025-04-03 14:19:14 -0300

    Adiciona funcionalidade aos arquivos

--- Atividade/javaatividade.js
+++ Atividade/javaatividade.js
@@ -47,3 +47,54 @@ function gasolina() {
 
 }
 
+//prato
+function prato() {
+    let peso = prompt("Qual o peso do seu prato : ")
+    let valorPeso = peso * 12.00
+    let valorFinal = valorPeso
+
+    alert("O preço do seu prato é : " + valorFinal + "Reais")
+
+}
+
+//Dias do ano
+function dias() {
+    let dia = prompt("Que dia é hoje: ");
+    if (dia > 30) { dia = 30 }
+    let mes = prompt("Que mês é hoje: ");
+    if (mes > 12) { mes = 12  }
+    let diasPorMes = 30;
+    let diasAnteriores = (mes - 1) * diasPorMes;
+    let totalDias = diasAnteriores + dia;
+    alert("ja se passaram " + totalDias + " Dias")
+}
+
+//nota
+function nota() {
+    let nota1 = parseFloat(prompt("Digite a primeira nota: "));
+    let nota2 = parseFloat(prompt("Digite a segunda nota: "));
+    let nota3 = parseFloat(prompt("Digite a terceira nota: "));
+    
+   
+    let peso1 = 1;
+    let peso2 = 2;
+    let peso3 = 3;
+    
+    
+    let mediaPonderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+    
+    alert("A média ponderada das notas é: " + mediaPonderada.toFixed(2)); 
+}
+
+function teste() {
+let quantidadePequena = prompt("Quantas camisetas pequenas :")
+let quantidadeMedia = prompt("Quantas camisetas medias :")
+let quantidadeGrande =  prompt("Quantas camisetas grandes :")
+let valorFinal = (quantidadePequena * 10) + (quantidadeMedia * 12) + (quantidadeGrande * 15)
+alert("O valor final é : " + valorFinal)
+}
+
+function plano(){
+    
+}
+
